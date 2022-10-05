d41705f64ce3abc583d8de69e894bf3870939722
Gautam Kumar <gkvc9696@gmail.com>
2022-10-05 15:28:59 +0530

    Rebased: Folder Structure and Fix: Netlify Issue

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const inputData = document.querySelector(".inputtext");
 const translateBtn = document.querySelector(".translatebtn");
 const translatedata = document.querySelector(".output");
-
+translatedata.style.display = "none";
 translateBtn.addEventListener("click", () => {
+  translatedata.style.display = "block";
   const API = `https://api.funtranslations.com/translate/minion.json?text=${inputData.value}`;
   fetch(API)
     .then((res) => res.json())
